99517e484783ecbb48e01bb11482c1836afaaf7f
Jungy <jungorend@gmail.com>
2022-12-01 13:20:04 -0500

    Day One First Problem

--- src/one.clj
+++ src/one.clj
@@ -1,1 +1,30 @@
-(ns one)
+(ns one
+  (:require [clojure.string :as s]))
+
+(def input (s/split (slurp "inputs/one") #"\n"))
+
+(defn first-elf
+  [x]
+  (loop [elf []
+         others x]
+    (cond (empty? others) [elf nil]
+          (= "" (first others)) [elf (rest others)]
+          :else (recur (conj elf (Integer/parseInt (first others))) (rest others)))))
+
+(defn elves
+  [x]
+  (loop [r []
+         i x]
+    (if (empty? i)
+      r
+      (let [elf (first-elf i)]
+        (recur (conj r (first elf)) (second elf))))))
+
+(defn elf-totals
+  [x]
+  (map #(apply + %) x))
+
+(->> (elves input)
+     elf-totals
+     (sort >)
+     first)
